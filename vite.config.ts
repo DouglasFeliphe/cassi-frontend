037b3861b313b43d2f7a21066494f1df69877286
DouglasFeliphe <douglas.feliphe@going2.com.br>
2025-06-12 12:14:58 -0300

    build: absulute imports config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import react from "@vitejs/plugin-react";
-import tailwind from "tailwindcss";
-import { defineConfig } from "vite";
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import tailwind from 'tailwindcss';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: './',
   css: {
     postcss: {
-      plugins: [tailwind()],
+      plugins: [tailwind],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
